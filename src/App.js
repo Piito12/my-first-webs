41c81ea0f90e02f5d6a8f16586f2f29492158ba1
Piito12 <sitthipong18748@gmail.com>
2022-09-20 13:29:12 +0700

    First Web page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button , Spinner , Input} from '@chakra-ui/react'
+import DisplayJohn, {Johnsprofile} from './components/DisplayJohn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Hello Mr.Oat</h1>
+      <div className='button'>
+      <Button colorScheme={'blue'} size= 'sm'>Button!</Button>
+      <Button colorScheme={'red'} size= 'lg'>Button!</Button>
+      <Button colorScheme={'green'} size= 'xl'>Button!</Button>
+      </div>
+      
+      <div className='spiner'>
+        <Spinner color='red.500' size={'sm'}></Spinner>
+        <Spinner color='blue' size={'xl'}></Spinner>
+        <Spinner color='green' size={'lg'}></Spinner>
+      </div>
+
+      <div className='input'>
+        <Input size={'lg'} type ='text' placeholder='Text'></Input>
+        <Input size={'lg'} type ='number' placeholder='Number'></Input>
+        <Input size={'lg'} type ='password' placeholder='password'></Input>
+      </div>
+
+      <div className='Ex-2'>
+        <DisplayJohn/>
+        <Johnsprofile/>
+      </div>
+      
     </div>
   );
 }
